6051844d66bbe1eb565663e26c4c77d0a1652f2d
angel <angel@LAPTOP-DN5VPBQO>
2025-01-24 16:03:36 +0100

    Segundo commit

--- proyectoDeGit/src/paquete1/claseEjemplo.java
+++ proyectoDeGit/src/paquete1/claseEjemplo.java
@@ -5,6 +5,7 @@ public class claseEjemplo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		System.out.println("Nuevo cambio");
 	}
 
 }
